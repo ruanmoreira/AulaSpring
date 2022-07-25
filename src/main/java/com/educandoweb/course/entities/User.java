bd29f4decec3d10de5bb584233dde33f794fa4bf
Ruan Moreira <ruaanmoreira2508@gmail.com>
2022-07-25 14:16:14 -0300

    Database, Security e JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,19 +1,29 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "tb_User")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
 	private String phone;
 	private String password;
 	
+	public User() {
+
+	}
 	public User(Long id, String nome, String email, String phone, String password) {
-		super();
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
@@ -61,22 +71,6 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return Objects.equals(id, other.id);
-	}
 	
 	
 }
